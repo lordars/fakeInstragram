ccde9bb46661e061bee3f0a651ac4dae98c8dda9
Sérgio Moura <smouracalmon@gmail.com>
2020-05-15 16:15:49 -0300

    Relacionar post com comentário

--- testes/testeModelPost.js
+++ testes/testeModelPost.js
@@ -1,12 +1,33 @@
-const { sequelize, Post } = require('../models');
+const { sequelize, Usuario, Post, Comentario } = require('../models');
 
-Post.findAll({include:'autor'}).then(
+Post.findByPk(1,
+    {include:
+        [
+            {
+                model: Usuario,
+                as: "autor",
+                include: "posts",
+                attributes:{
+                    exclude:['senha']
+                }
+            },
+            {
+                model:Comentario,
+                as:'comentarios',
+                include:'autor'
+            }
+        ]
+    }).then(
     data => {
-        console.log(data.map( u => u.toJSON()));
+        console.log(data.toJSON());
         sequelize.close();
     }
 )
-
+/**
+ * Levantar todos os POST. De Cada post trazer os comentários e
+ * de cada comentário, trazer o seu autor
+ * Também trazer os posts do autor do post em questão
+ */
 /**
  * Levantar todos os POST. De Cada post trazer os comentários e
  * de cada comentário, trazer o seu autor
